6c6bca5f265ab7c2c328807488e9f0783666d923
Tomas Jansson <tomas.jansson@gmail.com>
2022-11-23 22:20:43 +0100

    Failing build

---
+++ app/search/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type PageProps = {
+  searchParams: { q: string };
+};
+
+const Page = ({ searchParams }: PageProps) => {
+  return <div>Search</div>;
+};
+
+export default Page;
